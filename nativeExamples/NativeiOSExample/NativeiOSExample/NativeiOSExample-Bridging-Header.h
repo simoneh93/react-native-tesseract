b5808ff76e39d3b3216dde39fbcabb7453a727c7
Lee Brindley <leebrindley@gmail.com>
2019-10-30 14:53:38 +0000

    Added native ios project for testing native integration

---
+++ nativeExamples/NativeiOSExample/NativeiOSExample/NativeiOSExample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  NativeiOSExample-Bridging-Header.h
+//  NativeiOSExample
+//
+//  Created by Fendorio on 30/10/2019.
+//  Copyright © 2019 Fendorio. All rights reserved.
+//
+
+#ifndef NativeiOSExample_Bridging_Header_h
+#define NativeiOSExample_Bridging_Header_h
+
+#import "TesseractOCR.h"
+
+#endif /* NativeiOSExample_Bridging_Header_h */
